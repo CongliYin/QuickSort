f394f5b749e22a8083202c873c4544ddd3ccfe18
coliyin@163.com <coliyin@163.com>
2017-08-01 21:58:55 +0800

    QuickSort

---
+++ QuickSort.cpp
@@ -0,0 +1,54 @@
+//快速排序思想：
+//1、选择数组最右的元素作为基准
+//2、将大于基准的数和小于基准的数分成左右两部分，将基准放置于两部分的中间
+//3、对左右两部分递归调用快速排序
+
+#include <stdio.h> 
+int a[101],n;//定义全局变量，这两个变量需要在子函数中使用 
+void quicksort(int left,int right) 
+{ 
+    int i,j,t,temp; 
+    if(left > right) 
+       return; 
+                                
+    temp = a[left]; //temp中存的就是基准数 
+    i = left; 
+    j = right; 
+    while(i != j) 
+    { 
+                   //顺序很重要，要先从右边开始找 
+                   while(a[j]>=temp && i<j) 
+                            j--; 
+                   //再找左边的 
+                   while(a[i]<=temp && i<j) 
+                            i++; 
+                   //交换两个数在数组中的位置 
+                   if(i<j) 
+                   { 
+                            t = a[i]; 
+                            a[i] = a[j]; 
+                            a[j] = t; 
+                   } 
+    } 
+    //最终将基准数归位 
+    a[left] = a[i]; 
+    a[i] = temp; 
+                             
+    quicksort(left,i-1);//继续处理左边的，这里是一个递归的过程 
+    quicksort(i+1,right);//继续处理右边的 ，这里是一个递归的过程 
+} 
+int main() 
+{ 
+    int i,j,t; 
+    //读入数据 
+    scanf("%d",&n); 
+    for(i=0;i<n;i++) 
+      scanf("%d",&a[i]); 
+    quicksort(0,n-1); //快速排序调用 
+                             
+    //输出排序后的结果 
+    for(i=0;i<n;i++) 
+        printf("%d ",a[i]); 
+    getchar();getchar(); 
+    return 0; 
+} 
